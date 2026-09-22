b91c88df297bd4eb93a72956ca2d29ceec3ec9e9
Raydelis Hilario <raydelishilariomendez@gmail.com>
2026-09-22 18:42:43 -0400

    Agrega validaciones para campos de registro y login

---
+++ src/middlewares/validaciones.middleware.js
@@ -0,0 +1,17 @@
+export const validarCamposRegistro = async (req,res,next) => {
+    if (!req.body.nombre || !req.body.email || !req.body.password) {
+        return res.status(400).json({
+            error: "Los campos nombre, email y password son requeridos"
+        });
+    }
+    next();
+}
+
+export const validarCamposLogin = async (req,res,next) => {
+    if (!req.body.email || !req.body.password) {
+        return res.status(400).json({
+            error: "Los campos email y password son requeridos"
+        });
+    }
+    next();
+}
