23044aef915cad91f765b497cd7be10ac9201134
Raydelis Hilario <raydelishilariomendez@gmail.com>
2026-09-22 20:22:47 -0400

    agrega prestamos.controller

---
+++ src/controllers/prestamos.controller.js
@@ -0,0 +1,33 @@
+import { prisma } from "../db.js"
+
+export const crearPrestamo = async (req, res, next) => {
+	try {
+		
+	} catch (error) {
+		next(error)
+	}
+}
+
+export const devolverPrestamo = async (req, res, next) => {
+	try {
+		
+	} catch (error) {
+		next(error)
+	}
+}
+
+export const obtenerPrestamos = async (req, res, next) => {
+	try {
+		
+	} catch (error) {
+		next(error)
+	}
+}
+
+export const obtenerMisPrestamos = async (req, res, next) => {
+	try {
+		
+	} catch (error) {
+		next(error)
+	}
+}
